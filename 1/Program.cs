22835b4dd6460a9641830e277d1a496147110e10
babarchapri <babarhussain00931@gmail.com>
2025-04-16 19:48:24 +0500

    Add files via upload

---
+++ 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task1
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            int age = 25;
+            string name = "Babar";
+            
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+           
+        }
+    }
+}
